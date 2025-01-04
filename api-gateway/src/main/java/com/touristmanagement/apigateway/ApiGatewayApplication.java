9aeee655d81efdfe9e4b1577640a9eb23d309367
ajithak003 <93180124+ajithak003@users.noreply.github.com>
2025-01-04 23:30:13 +0530

    Add files via upload

--- api-gateway/src/main/java/com/touristmanagement/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/touristmanagement/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,15 @@ package com.touristmanagement.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
